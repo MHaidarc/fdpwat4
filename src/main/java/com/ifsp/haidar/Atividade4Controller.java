8fc18456848e6ac31ab7ce0cc83da706f53a7ade
maty carvalho <mateus.gcarvalho@proton.me>
2026-03-27 20:44:21 -0300

    exercicio 2

--- src/main/java/com/ifsp/haidar/Atividade4Controller.java
+++ src/main/java/com/ifsp/haidar/Atividade4Controller.java
@@ -20,4 +20,19 @@ public class Atividade4Controller {
         model.addAttribute("imc", imc);
         return "resultadoIMC.html";
     }
+
+    @GetMapping("/emprestimo")
+    public String emprestimo() {
+        return "formularioEmprestimo.html";
+    }
+
+    @PostMapping("/resultadoEmprestimo")
+    public String resultadoEmprestimo(@RequestParam double valor, @RequestParam double parcelas, @RequestParam double juros, Model model) {
+        double valorParcela = (valor * (1 + (juros / 100))) / parcelas;
+        model.addAttribute("valor", valor);
+        model.addAttribute("juros", juros);
+        model.addAttribute("parcelas", parcelas);
+        model.addAttribute("valorParcela", valorParcela);
+        return "resultadoEmprestimo.html";
+    }
 }
